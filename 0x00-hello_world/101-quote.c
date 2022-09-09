093bf1f1c842a421af81c0deed50b447db2d248f
Yugor Paul <yugorpaul@gmail.com>
2022-09-08 19:47:48 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,19 @@
 #include <unistd.h>
 
-/**
-* main - prints the standard error sandwiched around the standard output
-* Return: returns 1
+/*
+*
+* main - Prints a text
+*
+*
+*
+* Return: Always 1 (Success)
+*
 */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\"-Dora Korpar,2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n", 59);
 return (1);
 }
+
